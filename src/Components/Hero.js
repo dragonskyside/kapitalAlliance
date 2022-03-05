996346386b8023a41fdf2ee4fe612c10e7311368
skydragon <bwalikurunda@gmail.com>
2022-03-05 09:54:49 +0100

    font and style changes, social media icons

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div className="flex justify-between p-20 w-full h-screen bg-webphoto bg-cover bg-center">
+    <div className="flex justify-between p-20 w-full h-screen bg-webphoto bg-cover bg-center opacity-9">
       
       
 
         {/* Left side */}
         <div className="w-1/2 text-2xl font-bold">
             <h2 className='font-mono text-4xl mt-24 mx-4 text-yellowsun'>Kapital Alliance DAO</h2>
-            <p className='text-base font-mono my-12 mx-4 mb text-yellowsun'> A decentralized autonomous organization (DAO) for scalable global investments facilitated by blockchain technology. <br/> Community members  a.k.a 'kapitalists' contribute to collectively make investments in blockchain projects, stonks and high growth / cashflow positive assets. <br/> No more going solo,  missing out on opportunities or getting dumped on by whales. Now you can become a whale.<br/> Join us as we build the future of the internet with globally scalable opportunities for everyone. </p>
+            <p className='font-mono my-12 mx-4 mb text-yellowsun'> A decentralized autonomous organization (DAO) for scalable global investments facilitated by blockchain technology. <br/><br/> Community members  a.k.a 'kapitalists' contribute to collectively make investments in web3 projects and high growth, cash flow positive businesses. <br/><br/> No more going solo, missing out on opportunities or getting dumped on by whales. Invest as part of a community.<br/><br/> Join us as we build the future of the internet with globally scalable opportunities for everyone. </p>
             <img src="images/icons_btn-whitepaper.png" alt="NFT" className='w-48 m-4 py-24'></img>
         </div>
 
@@ -18,32 +18,32 @@ function Hero() {
         {/* Right side */}
 
         <div className=" w-1/2 text-2xl mt-24 text-white align font-mono center ml-48 font-bold">
-
+{/* 
           <div className="uppercase text-3xl mb-12">
             <h2>Why Join The Kapital Alliance DAO ?</h2>
-          </div>
-            <div className="">
+          </div> */}
+            {/* <div className="">
                 <h2 className='capitalize'>Access to deals</h2>
-                <p className='text-sm mb-6 font-bold'>Concentrated capital has access to more and higher quality deal flow than retail investors.
+                <p className='text-l mb-6 font-bold'>Concentrated capital has access to more and higher quality deal flow than retail investors.
                 </p>
             </div>
             <div className="">
                 <h2 className='capitalize'>Better terms</h2>
-                <p className='text-sm mb-4'>Getting in early and making high volume investments usually means obtaining favourable terms on deals</p>
-            </div>
+                <p className='text-l mb-4'>Getting in early and making high volume investments usually means obtaining favourable terms on deals</p>
+            </div> */}
 
             
-            <div className="">
+            {/* <div className="">
                 <h2 className='capitalize'>Community</h2>
-                <p className='text-sm mb-4'>Large and diverse community provides social support. Diversity of holders means access to a wider array of high quality ideas which translate to better investments. 
+                <p className='text-l mb-4'>Large and diverse community provides social support. Diversity of holders means access to a wider array of high quality ideas which translate to better investments. 
 
                 </p>
-            </div>
+            </div> */}
 
-            <div className="">
+            {/* <div className="">
                 <h2 className='capitalize font-bold'>Perks</h2>
-                <p className='text-sm mb-12'>First dip on deals, whitelisting for projects, discounts on goods and services with partner projects, airdrops if any, news, updates and informational material, NFTs.</p>
-            </div>
+                <p className='text-l mb-12'>First dip on deals, whitelisting for projects, discounts on goods and services with partner projects, airdrops if any, news, updates and informational material, NFTs.</p>
+            </div> */}
             
         </div>
 
--- src/Components/Nft.js
+++ src/Components/Nft.js
@@ -2,46 +2,44 @@ import React from 'react'
 
 function Nft() {
   return (
-    <div className="flex justify-between font-mono p-20 w-full h-screen bg-webphoto bg-cover bg-center">
+    <div className="flex justify-between font-mono p-20 w-full h-screen bg-webphoto bg-cover bold bg-center">
       
-      
+       {/* <div className="">
+       <img src="images/midas.webp" h-90 w-75 alt="NFT" className='w-48 m-4 py-24'></img></div> */}
 
         {/* Left side */}
         <div className="w-1/2 text-xl text-yellowsun align-center">
 
             <h2 className="uppercase text-yellosun text-4xl">NFT MINT </h2>
            
-            <p className='text-l m-4 '>Kapital Alliance DAO is launching a distinct line of NFTs to be called the MIDAS COLLECTION. It will consist of 2,500 beautifully designed art pieces  reflecting our vision. <br/> <br/> Midas was the legendary king of antiquity that turned all he touched to gold. The Midas NFT is the digital gold for our ecosystem that confers ownership and grants rights and priviledges to holders <br/> <br/> Mint is planned for March 15th 1800 GMT. Five hundred NFTs will be reserved for whitelisted entrants.</p>
-            <p className='text-l mt-4 mb-16 mx-4'> Join us as we build the future of global, scalable investment opportunities for everyone. </p>
+            <p className='text-l mt-4 mb-16'>As a first step towards its goals, the Kapital Alliance DAO is launching a distinct line of NFTs to be called the MIDAS COLLECTION. It will consist of 2,500 beautifully designed art pieces  at a reserve price of 0.5 ETH. Talented artists have been commisioned to produce works of the highest quality. <br/> <br/> Midas was the legendary king of antiquity that turned all he touched to gold. The Midas NFT is digital gold for our ecosystem that confers ownership and grants rights and priviledges to holders <br/> <br/> Mint is planned for March 18th at 1800 GMT. Five hundred NFTs will be reserved for whitelisted entrants. These are community members who apply early. <br/> Proceeds from the launch will be used to seed the treasury.<br/><br/>
+             Join us as we build the future of global, scalable investment opportunities for everyone. </p>
             {/* <img src="images/icons_btn-whitepaper.png" alt="NFT" className='w-48 m-4 py-4'></img> */}
-            <a href="#"><button class="bg-yellowsun hover:bg-white text-black font-bold py-2 px-4 rounded mb-16">Apply to WhiteList</button></a>
+            <a href="#"><button class="bg-yellowsun hover:bg-white text-black font-bold py-4 px-4 rounded mb-16">Apply to WhiteList</button></a>
             
 
-            <div className="text-white">
-                
-                
-            </div>
+            
         </div>
 
         {/* Right side */}
 
-        <div className=" w-1/2 text-2xl text-white align center font-bold ml-48">
+        {/* <div className=" w-1/2 text-2xl text-white align center ml-48">
             <div className="capitalize mb-4">
                 <h2>Next Steps after the MIDAS NFT Launch ...</h2>
             </div>
             <div className="">
                 <h2 className='capitalize'>Form a treasury</h2>
-                <p className='text-sm mb-8'>Proceeds from the NFT launch will be gathered up in ethereum and used to seed the treasury. Community NFT holders will be issued KA governance tokens via airdrop.
+                <p className='text-l mb-8'>Proceeds from the NFT launch will be gathered up in ethereum and used to seed the treasury. Community NFT holders will be issued KA governance tokens via airdrop.
                 </p>
             </div>
             <div className="">
                 <h2 className='capitalize'>Deploy funds to purchase assets</h2>
-                <p className='text-sm mb-8'>Core investment team or token holders present 
+                <p className='text-l mb-8'>Core investment team or token holders present 
                 propsals for investment and vote is taken. Disbursement for purchase is made after voting</p>
             </div>
             <div className="">
                 <h2 className='capitalize'>Shape the utility of the project</h2>
-                <p className='text-sm mb-12'>Midas token holders are treated as pioneers within the project as their contributions aid in liftoff.<br/>
+                <p className='text-l mb-12'>Midas token holders are treated as pioneers within the project as their contributions aid in liftoff.<br/>
                  They thus retain certain rights in governance
                      and have special treatment with respect to perks giveaways, whitelists  airdrops etc
 
@@ -50,9 +48,9 @@ function Nft() {
             {/* <div className="">
                 Join Us !
             </div> */}
-        </div>
+        {/* </div> */} 
 
-        <div className=""></div>
+       
 
 
 
