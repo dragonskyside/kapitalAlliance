adec6183f4cb45898023abb24039a47bb49993d4
skydragon <bwalikurunda@gmail.com>
2022-03-10 23:39:41 +0100

    icon links

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -43,9 +43,9 @@ function Header(props) {
        <Link href="#" className="inline-block p-3 text-stone hover:text-yellowsun mr-20 text-lg">NFT Whitelist</Link>
        {/* <Link href="#" className="inline-block p-3 text-stone hover:text-yellowsun mr-20 text-lg">Translations</Link> */}
        <Link href="#" className="inline-block p-3 text-stone hover:text-yellowsun mr-20">Download <br/>Whitepaper</Link>
-       <Link href="#" className="mr-5 mt-5 xl"><FaTwitter/></Link>
-       <Link href="#" className="mr-5 mt-5"><FaTelegram /></Link>
-       <Link href="#" className="mr-5 mt-5"><FaDiscord/></Link>
+       <a href="https://twitter.com/kapitalalliance" className="mr-5 mt-5 xl"><FaTwitter/></a>
+       <a href="#" className="mr-5 mt-5"><FaTelegram /></a>
+       <a href="https://discord.gg/8rmUQYq6" className="mr-5 mt-5"><FaDiscord/></a>
 
       
       
